412d5ec14dd936f629617e990aa0f53cb7d7284b
Peter-John Welcome <Peter-John.Welcome@standardbank.co.za>
2017-03-27 09:09:16 +0200

    Work around for using lambdas in A library with java 1.8 and added Internet permission

--- lifxhttpandroidlibrary/src/main/java/com/multimeleon/android/lifxhttpandroidlibrary/Repo/LifxClientPresenter.java
+++ lifxhttpandroidlibrary/src/main/java/com/multimeleon/android/lifxhttpandroidlibrary/Repo/LifxClientPresenter.java
@@ -1,9 +1,5 @@
 package com.multimeleon.android.lifxhttpandroidlibrary.Repo;
 
-import com.multimeleon.android.lifxhttpandroidlibrary.Models.SingleLight.Light;
-
-import java.util.List;
-
 /**
  * Created by Peter-John on 2017-03-25.
  * LifxHttpAndroid
